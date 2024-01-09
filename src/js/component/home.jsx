87f564e5b6520b1c3b8423ea5eab84f63d6b367e
Mercedes0708 <144907375+Mercedes0708@users.noreply.github.com>
2024-01-09 18:24:12 +0000

    feat: Jumbotrom terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,12 @@
 import React from "react";
-
+import { Jumbotron } from "./Jumbotron.jsx";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<Jumbotron/>
 	);
 };
 
