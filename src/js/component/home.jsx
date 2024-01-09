19461b1e7b0f615bb739425ba9c3933c6da0d452
Matteo0310 <145265011+Matteo0310@users.noreply.github.com>
2024-01-09 20:19:01 +0000

    feat: Cards actualizadas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 
 //include images into your bundle
-import { Card } from "./Card";
+import { Cards } from "./Cards.jsx";
 //create your first component
 const Home = () => {
+	// Codigo para crear el placeholder de las 4 cards
+	let arrayPlaceholder = [
+		{ title: 'Bootstrap', subtitle: 'Bootstrap is a free front-end framework, with the purpose of making web development faster and easier.', label: 'Bootstrap', image: 'https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-social.png' },
+		{ title: 'Javascript', subtitle: 'JavaScript (JS) is a cross-platform, object-oriented programming language used by developers to make web pages interactive.', label: 'Learn Javascript', image: 'https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg' },
+		{ title: 'React', subtitle: 'React is a JavaScript library developed by Facebook which, among other things, was used to build Instagram.com.', label: 'Learn React', image: 'https://www.patterns.dev/img/reactjs/react-logo@3x.svg' },
+		{ title: 'Python', subtitle: 'Somos el mejor grupo de 4Geeks Academy gracias a CarlosAtanesxx.', label: 'Learn Python', image: 'https://pbs.twimg.com/media/FHAiKCeXsAASzF8.jpg:large' },
+	];
+
 	return (
-		<Card/>
+		<>
+			
+				<Cards valores={arrayPlaceholder} />
+		
+		</>
 	);
 };
 
